0ea6eafef1aff717391f056baafff05af934c3f9
willwhop <146436965+willwhop@users.noreply.github.com>
2024-11-25 19:03:28 +0000

    made the interactable object change colour and shape as well as change camera from third person to first person

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,20 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 public class PlayerControls : MonoBehaviour
 {
+    public Camera Camera1;
+    public Camera Camera2;
+    public Material mat1;
+    public Mesh mesh1;
+    public Material mat2;
+    public Mesh mesh2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Camera2.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Input.GetKeyDown();
+        GameObject[] InteractableObjects = GameObject.FindGameObjectsWithTag("Interactable");
+        if (Input.GetKeyDown("1")) {
+            Camera1.enabled = false;
+            Camera2.enabled = true;
+            foreach (GameObject Obj in InteractableObjects) {
+                Obj.GetComponent<Renderer>().material = mat1;
+                Obj.GetComponent<MeshFilter>().mesh = mesh1;
+            }
+        }
+        if (Input.GetKeyDown("2")) {
+            Camera1.enabled = true;
+            Camera2.enabled = false;
+            foreach (GameObject Obj in InteractableObjects) {
+                Obj.GetComponent<Renderer>().material = mat2;
+                Obj.GetComponent<MeshFilter>().mesh = mesh2;
+               
+
+
+
+            }
+        }
         //xrinput
     }
 }
