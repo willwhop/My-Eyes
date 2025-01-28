b96b1e3b98e1264da1bc8fe8e4f0c330614dec4a
willwhop <146436965+willwhop@users.noreply.github.com>
2025-01-28 12:21:26 +0000

    more updates

--- Assets/Scripts/Will/HouseSpawner.cs
+++ Assets/Scripts/Will/HouseSpawner.cs
@@ -7,21 +7,23 @@ public class HouseSpawner : MonoBehaviour
 {
     private float timer = 0;
     public GameObject MovingNPCPrefab;
-    private GameObject[] NPCAmount;
+    private int NPCAmount;
     void Start()
     {
-        NPCAmount = GameObject.FindGameObjectsWithTag("MovingNPCs");
+       // NPCAmount = GameObject.FindGameObjectsWithTag("MovingNPCs");
     }
     // Update is called once per frame
     void Update()
     {
-        if (NPCAmount.Length >= 2) {
+        if (NPCAmount > 2) {
+            
         }
         timer += Time.deltaTime;
-        if (NPCAmount.Length < 2) {
+        if (NPCAmount < 2) {
             if (timer >= 5f) {
                 Instantiate(MovingNPCPrefab, gameObject.transform);
                 timer = 0;
+                NPCAmount += 1;
             }
         }
     }
--- Assets/Scripts/Will/MovableNPCs.cs
+++ Assets/Scripts/Will/MovableNPCs.cs
@@ -26,6 +26,14 @@ public class MovableNPCs : MonoBehaviour {
     private Vector3 leftAngle2;
     private Vector3 rightAngle1;
     private Vector3 rightAngle2;
+    private Vector3 randLocation;
+    private NavMeshHit NavHit;
+
+    private int health;
+    private bool canhit;
+
+    bool notHorrorEyesischecked;
+    bool HorrorEyesIsChecked;
     // Start is called before the first frame update
     void Start() {
         //houses = GameObject.FindGameObjectsWithTag("house");
@@ -45,150 +53,165 @@ public class MovableNPCs : MonoBehaviour {
     //    }
     //}
     // Update is called once per frame
+    private void horrorEyes() {
 
-    
-    void Update() {
-        timer += Time.deltaTime;
-        Vector3 randLocation = transform.position + UnityEngine.Random.insideUnitSphere * WalkDistance;
-        NavMeshHit NavHit;
-        
-        //if player has horror eyes
-        if (Horroreyes.transform.IsChildOf(PlayerEyesCollisionBox.transform)) {
-            RaycastHit hit;
-            leftAngle1 = Quaternion.Euler(0, 50, 0) * transform.forward;
-            leftAngle2 = Quaternion.Euler(0, 25, 0) * transform.forward;
-            rightAngle1 = Quaternion.Euler(0, -50, 0) * transform.forward;
-            rightAngle2 = Quaternion.Euler(0, -25, 0) * transform.forward;
-            
-            //all rays for looking around
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1000)) {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.blue);
-                if (hit.collider.CompareTag("playerCharacter")) {
-                    if (hit.distance < 1f) {
-                        Npc.speed = 0;
-                        //play animation
-                        Debug.Log("hit player 5");
-                        gameObject.transform.LookAt(hit.collider.transform);
-                    }
-                    //check is attack animation currently playing 
-                    else if(hit.distance > 1f) {
-                        Npc.SetDestination(hit.collider.transform.position);
-                        gameObject.transform.LookAt(hit.collider.transform);
-                        Npc.speed = 3.5f;
-                    }
-                }                
-            }           
-            else if (Physics.Raycast(transform.position, transform.TransformDirection(leftAngle1), out hit, 1000)) {
-                Debug.DrawRay(transform.position, transform.TransformDirection(leftAngle1) * hit.distance, Color.blue);
-                if (hit.collider.CompareTag("playerCharacter")) {
-                    if (hit.distance < 1f) {
-                        Debug.Log("hit player 1");
-                        Npc.speed = 0;
-                        //play animation
-                        gameObject.transform.LookAt(hit.collider.transform);
-                    }
-                    //check is attack animation currently playing 
-                    else if (hit.distance > 1f) {
-                        Npc.SetDestination(hit.collider.transform.position);
-                        gameObject.transform.LookAt(hit.collider.transform);
-                        Npc.speed = 3.5f;
-                    }
+        canhit = true;
+
+        RaycastHit hit;
+        leftAngle1 = Quaternion.Euler(0, 50, 0) * transform.forward;
+        leftAngle2 = Quaternion.Euler(0, 25, 0) * transform.forward;
+        rightAngle1 = Quaternion.Euler(0, -50, 0) * transform.forward;
+        rightAngle2 = Quaternion.Euler(0, -25, 0) * transform.forward;
+
+        //all rays for looking around
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1000)) {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.blue);
+            if (hit.collider.CompareTag("playerCharacter")) {
+                if (hit.distance < 1f) {
+                    Npc.speed = 0;
+                    //play animation
+                    Debug.Log("hit player 5");
+                    gameObject.transform.LookAt(hit.collider.transform);
+                }
+                //check is attack animation currently playing 
+                else if (hit.distance > 1f) {
+                    Npc.SetDestination(hit.collider.transform.position);
+                    gameObject.transform.LookAt(hit.collider.transform);
+                    Npc.speed = 3.5f;
                 }
             }
-            else if (Physics.Raycast(transform.position, transform.TransformDirection(leftAngle2), out hit, 1000)) {
-                Debug.DrawRay(transform.position, transform.TransformDirection(leftAngle2) * hit.distance, Color.blue);
-                if (hit.collider.CompareTag("playerCharacter")) {
-                    if (hit.distance < 1f) {
-                        Npc.speed = 0;
-                        //play animation
-                        Debug.Log("hit player 2");
-                        gameObject.transform.LookAt(hit.collider.transform);
-                    }
-                    //check is attack animation currently playing 
-                    else if (hit.distance > 1f) {
-                        Npc.SetDestination(hit.collider.transform.position);
-                        gameObject.transform.LookAt(hit.collider.transform);
-                        Npc.speed = 3.5f;
-                    }
+        }
+        else if (Physics.Raycast(transform.position, transform.TransformDirection(leftAngle1), out hit, 1000)) {
+            Debug.DrawRay(transform.position, transform.TransformDirection(leftAngle1) * hit.distance, Color.blue);
+            if (hit.collider.CompareTag("playerCharacter")) {
+                if (hit.distance < 1f) {
+                    Debug.Log("hit player 1");
+                    Npc.speed = 0;
+                    //play animation
+                    gameObject.transform.LookAt(hit.collider.transform);
+                }
+                //check is attack animation currently playing 
+                else if (hit.distance > 1f) {
+                    Npc.SetDestination(hit.collider.transform.position);
+                    gameObject.transform.LookAt(hit.collider.transform);
+                    Npc.speed = 3.5f;
                 }
             }
-            else if (Physics.Raycast(transform.position, transform.TransformDirection(rightAngle1), out hit, 1000)) {
-                Debug.DrawRay(transform.position, transform.TransformDirection(rightAngle1) * hit.distance, Color.blue);
-                if (hit.collider.CompareTag("playerCharacter")) {
-                    if (hit.distance < 1f) {
-                        Npc.speed = 0;
-                        Debug.Log("hit player 3");
-                        gameObject.transform.LookAt(hit.collider.transform);
-                        //play animation
-                    }
-                    //check is attack animation currently playing 
-                    else if (hit.distance > 1f) {
-                        Npc.SetDestination(hit.collider.transform.position);
-                        gameObject.transform.LookAt(hit.collider.transform);
-                        Npc.speed = 3.5f;
-                    }
+        }
+        else if (Physics.Raycast(transform.position, transform.TransformDirection(leftAngle2), out hit, 1000)) {
+            Debug.DrawRay(transform.position, transform.TransformDirection(leftAngle2) * hit.distance, Color.blue);
+            if (hit.collider.CompareTag("playerCharacter")) {
+                if (hit.distance < 1f) {
+                    Npc.speed = 0;
+                    //play animation
+                    Debug.Log("hit player 2");
+                    gameObject.transform.LookAt(hit.collider.transform);
+                }
+                //check is attack animation currently playing 
+                else if (hit.distance > 1f) {
+                    Npc.SetDestination(hit.collider.transform.position);
+                    gameObject.transform.LookAt(hit.collider.transform);
+                    Npc.speed = 3.5f;
                 }
             }
-            else if (Physics.Raycast(transform.position, transform.TransformDirection(rightAngle2), out hit, 1000)) {
-                Debug.DrawRay(transform.position, transform.TransformDirection(rightAngle2) * hit.distance, Color.blue);
-                if (hit.collider.CompareTag("playerCharacter")) {
-                    if (hit.distance < 1f) {
-                        Npc.speed = 0;
-                        //play animation
-                        Debug.Log("hit player 4");
-                        gameObject.transform.LookAt(hit.collider.transform);
-                    }
-                    //check is attack animation currently playing 
-                    else if (hit.distance > 1f) {
-                        Npc.SetDestination(hit.collider.transform.position);
-                        gameObject.transform.LookAt(hit.collider.transform);
-                        Npc.speed = 3.5f;
-                    }
+        }
+        else if (Physics.Raycast(transform.position, transform.TransformDirection(rightAngle1), out hit, 1000)) {
+            Debug.DrawRay(transform.position, transform.TransformDirection(rightAngle1) * hit.distance, Color.blue);
+            if (hit.collider.CompareTag("playerCharacter")) {
+                if (hit.distance < 1f) {
+                    Npc.speed = 0;
+                    Debug.Log("hit player 3");
+                    gameObject.transform.LookAt(hit.collider.transform);
+                    //play animation
+                }
+                //check is attack animation currently playing 
+                else if (hit.distance > 1f) {
+                    Npc.SetDestination(hit.collider.transform.position);
+                    gameObject.transform.LookAt(hit.collider.transform);
+                    Npc.speed = 3.5f;
                 }
             }
-            if (CanMove == true) {
-                if (NavMesh.SamplePosition(randLocation, out NavHit, 20f, NavMesh.AllAreas)) {
-                    NewDestination = NavHit.position;
-                    CanMove = false;
+        }
+        else if (Physics.Raycast(transform.position, transform.TransformDirection(rightAngle2), out hit, 1000)) {
+            Debug.DrawRay(transform.position, transform.TransformDirection(rightAngle2) * hit.distance, Color.blue);
+            if (hit.collider.CompareTag("playerCharacter")) {
+                if (hit.distance < 1f) {
+                    Npc.speed = 0;
+                    //play animation
+                    Debug.Log("hit player 4");
+                    gameObject.transform.LookAt(hit.collider.transform);
+                }
+                //check is attack animation currently playing 
+                else if (hit.distance > 1f) {
+                    Npc.SetDestination(hit.collider.transform.position);
+                    gameObject.transform.LookAt(hit.collider.transform);
+                    Npc.speed = 3.5f;
                 }
             }
-            if (CanMove == false) {
-                if (timer >= 2) {
-                    CanMove = true;
-                    Debug.Log(CanMove);
-                    timer = 0;
-                    timerWait = UnityEngine.Random.Range(2, 7);
-                }
+        }
+        if (CanMove == true) {
+            if (NavMesh.SamplePosition(randLocation, out NavHit, 20f, NavMesh.AllAreas)) {
+                NewDestination = NavHit.position;
+                CanMove = false;
+
             }
-            else {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
-                Debug.DrawRay(transform.position, transform.TransformDirection(leftAngle1) * 1000, Color.red);
-                Debug.DrawRay(transform.position, transform.TransformDirection(leftAngle2) * 1000, Color.red);
-                Debug.DrawRay(transform.position, transform.TransformDirection(rightAngle1) * 1000, Color.red);
-                Debug.DrawRay(transform.position, transform.TransformDirection(rightAngle2) * 1000, Color.red);
-                //Npc.SetDestination(selectedHouse.transform.position);                
+        }
+        if (CanMove == false) {
+            if (timer >= 2) {
+                CanMove = true;
+                Debug.Log(CanMove);
+                timer = 0;
+                timerWait = UnityEngine.Random.Range(2, 7);
                 Npc.SetDestination(NewDestination);
-                Npc.speed = 3.5f;
-            }            
+            }
         }
         else {
-            if (CanMove == true) {
-                if (NavMesh.SamplePosition(randLocation, out NavHit, 20f, NavMesh.AllAreas)) {
-                    NewDestination = NavHit.position;
-                    CanMove = false;
-                }
-            }
-            if (CanMove == false) {
-                if (timer >= 2) {
-                    CanMove = true;
-                    Debug.Log(CanMove);
-                    timer = 0;
-                    timerWait = UnityEngine.Random.Range(2, 7);
-                }
-            }
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
+            Debug.DrawRay(transform.position, transform.TransformDirection(leftAngle1) * 1000, Color.red);
+            Debug.DrawRay(transform.position, transform.TransformDirection(leftAngle2) * 1000, Color.red);
+            Debug.DrawRay(transform.position, transform.TransformDirection(rightAngle1) * 1000, Color.red);
+            Debug.DrawRay(transform.position, transform.TransformDirection(rightAngle2) * 1000, Color.red);
+            //Npc.SetDestination(selectedHouse.transform.position);                
             Npc.SetDestination(NewDestination);
-            //Npc.SetDestination(selectedHouse.transform.position);
             Npc.speed = 3.5f;
+        }
+
+
+    }
+    private void notHorrorEyes() {
+
+        if (CanMove == true) {
+            if (NavMesh.SamplePosition(randLocation, out NavHit, 20f, NavMesh.AllAreas)) {
+                NewDestination = NavHit.position;
+                CanMove = false;
+            }
+        }
+        if (CanMove == false) {
+            if (timer >= 2) {
+                CanMove = true;
+                Debug.Log(CanMove);
+                timer = 0;
+                timerWait = UnityEngine.Random.Range(2, 7);
+            }
+        }
+        Npc.SetDestination(NewDestination);
+        //Npc.SetDestination(selectedHouse.transform.position);
+        Npc.speed = 3.5f;
+
+
+
+    }
+    void Update() {
+        timer += Time.deltaTime;
+        randLocation = transform.position + UnityEngine.Random.insideUnitSphere * WalkDistance;
+
+
+        //if player has horror eyes
+        if (Horroreyes.transform.IsChildOf(PlayerEyesCollisionBox.transform)) {
+            horrorEyes();
+        }
+        else {
+            notHorrorEyes();
         }
     }
 }
