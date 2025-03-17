bc7cf7b93305d66886972667a8db0f262197b6a4
s1308263 <s1308263@glos.ac.uk>
2025-03-17 22:26:28 +0000

    Jake update play test

---
+++ Assets/Scripts/Jacob/EyeScripts/EyeScript.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class EyeScript : MonoBehaviour {
+
+    [SerializeField] private GameObject scaryEyes, cuteEyes, eyeFilter;
+    [SerializeField] private Material pinkFilter, blackFilter; 
+    bool boolScary, boolCute;
+
+    private void OnTriggerEnter(Collider collider) {
+        //check if eyes are in trigger
+        if (collider.transform.CompareTag("ScaryEyes")) {
+            //set prefab
+            scaryEyes = collider.gameObject;
+            boolScary = true;
+        }
+        if (collider.transform.CompareTag("CuteEyes")) {
+            //set prefab
+            cuteEyes = collider.gameObject;
+            boolCute = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collider) {
+        //check if eyes are out of trigger
+        if (collider.transform.CompareTag("ScaryEyes")) {
+            boolScary = false;
+        }
+        if (collider.transform.CompareTag("CuteEyes")) {
+            boolCute = false;
+        }
+    }
+
+    public void OnEyes() {
+        if (boolScary == true) {
+            //Add scary eyes functionality here
+
+            //Hide scary eyes mesh
+            scaryEyes.GetComponent<MeshRenderer>().enabled = false;
+
+            //Debug eye filter
+            eyeFilter.gameObject.SetActive(enabled = true);
+            eyeFilter.GetComponent<Renderer>().material = blackFilter;
+        }
+        if (boolCute == true) {
+            //Add cute eyes funtionality here
+
+            //Hide cute eyes mesh
+            cuteEyes.GetComponent<MeshRenderer>().enabled = false;
+
+            //Debug eye filter
+            eyeFilter.gameObject.SetActive(enabled = true);
+            eyeFilter.GetComponent<Renderer>().material = pinkFilter;
+        }
+    }
+
+    public void OffEyes() {
+        //Show eyes mesh
+        scaryEyes.GetComponent<MeshRenderer>().enabled = true;
+        cuteEyes.GetComponent <MeshRenderer>().enabled = true;
+        //Disable filter
+        eyeFilter.gameObject.SetActive(enabled = false);
+    }
+}
--- Assets/Scripts/Jacob/PlayerMovement/CharacterMovement.cs
+++ Assets/Scripts/Jacob/PlayerMovement/CharacterMovement.cs
@@ -1,53 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Data.SqlTypes;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour {
 
     [Header("Movement")]
-    public float moveSpeed;
-
-    public float groundDrag;
-
-    public float jumpForce;
-    public float jumpCooldown;
-    public float airMultiplier;
-    bool canJump;
+    [SerializeField] private float moveSpeed, groundDrag, jumpForce, jumpCooldown, airMultiplier;
+    [SerializeField] private Transform orientation;
 
     [Header("Controller Inputs")]
     PlayerInput playerInput;
 
-    public InputActionReference LJoyInput;
-    public InputActionReference jump;
+    [SerializeField] private InputActionReference LJoyInput;
+    [SerializeField] private InputActionReference jump;
 
     [Header("Ground Check")]
-    public float playerHeight;
-    public LayerMask ground;
-    bool grounded;
+    [SerializeField] private LayerMask ground;
 
-    public Transform orientation;
+    float playerHeight = 2;
+    bool grounded;
+    bool canJump;
 
     Vector2 move;
-
     Vector3 moveDirection;
 
     Rigidbody rb;
 
     private void Start() {
         playerInput = GetComponent<PlayerInput>();
-        move = LJoyInput.action.ReadValue<Vector2>();
 
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
 
     private void Update() {
+        move = LJoyInput.action.ReadValue<Vector2>();
 
         //ground check
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.32f, ground);
 
         //MoveInput();
         SpeedControl();
@@ -68,17 +57,15 @@ public class CharacterMovement : MonoBehaviour {
     }
 
     private void OnEnable() {
-        
         jump.action.started += Jump;
     }
 
     private void OnDisable() {
-        
         jump.action.started -= Jump;
     }
 
-    private void MoveInput(InputAction.CallbackContext moveContext) {
-        if(canJump && grounded) {
+    private void MoveInput(InputAction.CallbackContext context) {
+        if (canJump && grounded) {
             canJump = false;
             Invoke(nameof(ResetJump), jumpCooldown);
         }
@@ -89,8 +76,13 @@ public class CharacterMovement : MonoBehaviour {
         moveDirection = orientation.forward * move.y + orientation.right * move.x;
 
         //on ground
-        if (grounded && move.x == 0 && move.y == 0) {
+        if (grounded) {
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+
+        //in air
+        else if (!grounded) {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
         }
     }
 
@@ -98,15 +90,19 @@ public class CharacterMovement : MonoBehaviour {
         Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
         //limit velocity if needed
-        Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
-        rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        if (flatVelocity.magnitude > moveSpeed) {
+            Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }
     }
 
     private void Jump(InputAction.CallbackContext jumpContext) {
-        //reset y velocity
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if (grounded) {
+            //reset y velocity
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
     private void ResetJump() {
