4ddccaa2b34ff434106e66f9214d92805fa2df23
Jacob Thorley <s1308263@glos.ac.uk>
2025-04-09 19:55:37 +0100

    added basic item pickup to player

---
+++ Assets/Scripts/Jacob/ItemScripts/ItemFollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemFollowPlayer : MonoBehaviour {
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform itemAnchorPoint;
+    [SerializeField] private AudioClip collectAudio;
+    AudioSource source;
+
+    private void OnTriggerEnter(Collider collider) {
+        if (collider.CompareTag("Player")) {
+            player = collider.gameObject;
+            gameObject.transform.parent = collider.transform;
+            itemAnchorPoint = player.transform.GetChild(0).gameObject.transform;
+            gameObject.transform.position = itemAnchorPoint.transform.position;
+            source = GetComponentInParent<AudioSource>();
+            source.PlayOneShot(collectAudio);
+        }
+    }
+}
