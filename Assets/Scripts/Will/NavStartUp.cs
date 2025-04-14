7c3dba7d09f33b62af759f417cf2bb4a1cb55f3b
willwhop <146436965+willwhop@users.noreply.github.com>
2025-04-14 18:56:30 +0100

    Movable AI code has been modified and dog resetting player should work now

--- Assets/Scripts/Will/NavStartUp.cs
+++ Assets/Scripts/Will/NavStartUp.cs
@@ -9,21 +9,17 @@ public class NavStartUp : MonoBehaviour
     private GameObject dog;
     public GameObject dogbowl;
     public GameObject scrapbot;
-    public GameObject[] dogpoints;
+    public Transform[] dogpoints;
 
     // Start is called before the first frame update
     void Start()
     {
         dog = GameObject.FindGameObjectWithTag("Dog");
-        foreach (GameObject point in dogpoints) {
+        foreach (Transform point in dogpoints) {
             dog.GetComponent<MovableNPCs>().PatrolPoints.Add(point);
         }
         dog.GetComponent<MovableNPCs>().questscript = scrapbot.GetComponent<QuestGiverNPC>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
