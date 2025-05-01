0715b913eeea92151b06afc262063d0e7f75e6a7
willwhop <146436965+willwhop@users.noreply.github.com>
2025-05-01 20:46:30 +0100

    exit to main menu added

--- Assets/Scripts/Will/Menus/NextLevel.cs
+++ Assets/Scripts/Will/Menus/NextLevel.cs
@@ -11,6 +11,9 @@ public class NextLevel : MonoBehaviour
     public void NextArea() {
         SceneManager.LoadScene("Area2");
     }
+    public void EndGame() {
+        SceneManager.LoadScene("Main Menu");
+    }
     public void Quit() {
         Application.Quit();
     }
